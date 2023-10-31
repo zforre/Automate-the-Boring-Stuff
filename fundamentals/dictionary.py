8e39a44f566cb25cc12bea6786cc1702d0851b44
zforre <zachforre@gmail.com>
2023-10-31 15:21:31 -0400

    takes notes on dictionaries

---
+++ fundamentals/dictionary.py
@@ -0,0 +1,21 @@
+# myCat = {'size': 'fat', 'color': 'gray', 'disposition': 'loud'}
+
+# print(myCat['size'])
+# print(f'My cat has {myCat["color"]} fur')
+
+# spam = {12345: 'Luggage Combination', 42: 'The Answer'}
+
+# spam = ['cats', 'dogs', 'moose']
+# bacon = ['dogs', 'moose', 'cats']
+# print(spam == bacon)
+
+# eggs = {'name': 'Zophie', 'species': 'cat', 'age': '8'}
+# ham = {'species': 'cat', 'age': '8', 'name': 'Zophie'}
+# print(eggs == ham)
+
+# throws error because color key does not exist
+# spam = {'name': 'Zophie', 'age': 7}
+# print(spam['color'])
+
+
+
