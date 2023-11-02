a3fcf20000178872488df3a2433d73014392222f
zforre <zachforre@gmail.com>
2023-11-01 20:50:15 -0400

    continues notes on dictionary methods

--- fundamentals/dictMethods.py
+++ fundamentals/dictMethods.py
@@ -15,3 +15,28 @@
 # for k, v in spam.items():
 #     print(f'Key: {k} Value: {str(v)}')
 
+spam = {'name': 'Kewpie', 'age': 2}
+
+# print('name' in spam.keys())
+# print('Kewpie' in spam.values())
+# print('color' in spam.keys())
+# print('color' not in spam.keys())
+
+# get() method
+picnicItems = {'apples':5, 'cups': 2}
+print('I am bringing ' + str(picnicItems.get('cups', 0)) + ' cups.')
+print('I am bringing ' + str(picnicItems.get('eggs', 0)) + ' eggs.')
+
+# setdefault() method
+spam = {'name': 'Pooka', 'age': 5}
+print(spam.setdefault('color', 'black'))
+print(spam)
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+print(count)
