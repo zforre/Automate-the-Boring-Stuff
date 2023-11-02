b6cac34de5f1379d59c6f8cf88785ab86ff62b90
zforre <zachforre@gmail.com>
2023-11-01 20:35:49 -0400

    takes notes on dictionary methods

---
+++ fundamentals/dictMethods.py
@@ -0,0 +1,17 @@
+# spam = {'color':'red', 'age': 42}
+
+# for v in spam.values():
+#     print(v)
+
+# for k in spam.keys():
+#     print(k)
+
+# for i in spam.items():
+#     print(i)
+
+# print(spam.keys())
+# print(list(spam.keys()))
+
+# for k, v in spam.items():
+#     print(f'Key: {k} Value: {str(v)}')
+
